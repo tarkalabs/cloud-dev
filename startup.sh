de11a52b7403ed23f4788da4849b0aad868ccbb2
Madhava Reddy SV <madhav@tarkalabs.com>
2023-01-13 19:53:47 -0400

    service account for ide pod update

--- startup.sh
+++ startup.sh
@@ -7,22 +7,27 @@ curl -o /root/.ssh/authorized_keys "$PERSON_PUBLIC_KEY_URL"
 # configures kubeconfig
 aws eks update-kubeconfig --region us-east-1 --name cloud-dev --alias cloud-dev
 
-# changing directory to tilt
-cd tilt
+# cloning cloud-dev infra source code
+if [[ -d "cloud-dev" && -d "cloud-dev/.git" ]]; then
+  echo "Cloud dev repo already exists!"
+else
+  git clone https://github.com/tarkalabs/cloud-dev.git
+fi
 
 # we can use git submodules and can keep all at one place with different branches
 # Pulling frontend
-# check if folder already exists and skip
-git clone https://github.com/SVMadhavaReddy/cloud-dev-frontend.git
-
-# tilt up
+if [[ -d "cloud-dev-frontend" && -d "cloud-dev-frontend/.git" ]]; then
+  echo "Frontend repo already exists!"
+else
+  git clone https://github.com/SVMadhavaReddy/cloud-dev-frontend.git
+fi
 
 # Pulling backend
-git clone https://github.com/SVMadhavaReddy/cloud-dev-backend.git
-# simple flaskapp
-
-# Pulling other required services
-# follow above approach
+if [[ -d cloud-dev-frontend && -d "cloud-dev-backend/.git" ]]; then
+  echo "Backend repo already exists!"
+else
+  git clone https://github.com/SVMadhavaReddy/cloud-dev-backend.git
+fi
 
 # running tilt up
 # where should we have tilt file?
