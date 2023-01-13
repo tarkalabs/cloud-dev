4f1cc6d02aa1b83b1af4b1ce32a7f7319359a834
Madhava Reddy SV <madhav@tarkalabs.com>
2023-01-13 16:13:25 -0400

    tilt initial update with kubectl_build

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,9 @@ RUN curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/in
 RUN apt install -y docker \
   && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
+RUN wget -O /tmp/kubectl-buildkit.deb https://github.com/vmware-tanzu/buildkit-cli-for-kubectl/releases/download/v0.1.6/kubectl-buildkit_0.1.6_amd64.deb \
+  && dpkg -i /tmp/kubectl-buildkit.deb && rm -f /tmp/kubectl-buildkit.deb
+
 COPY startup.sh /root/startup.sh
 
 CMD [ "/root/startup.sh" ]
--- startup.sh
+++ startup.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+set -xe
 
 # Configures user public keys for ssh
 curl -o /root/.ssh/authorized_keys "$PERSON_PUBLIC_KEY_URL"
 
-# Pulling backend
+# changing directory to tilt
+cd tilt
 
+# we can use git submodules and can keep all at one place with different branches
 # Pulling frontend
+# check if folder already exists and skip
+git clone git@github.com:SVMadhavaReddy/cloud-dev-frontend.git
+
+tilt up
+
+# Pulling backend
+# git clone git@github.com:SVMadhavaReddy/cloud-dev-backend.git
+# simple flaskapp
 
 # Pulling other required services
+# follow above approach
 
 # running tilt up
+# where should we have tilt file?
 
 # Always has to run last
 /usr/sbin/sshd -D
